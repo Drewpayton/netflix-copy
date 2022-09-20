bf524137a142d39e49f4a216ee35407b1343ceeb
Drew Payton <drewpayton18@gmail.com>
2022-09-20 18:12:03 -0500

    adding a login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -18,6 +18,28 @@ router.post("/register", async (req, res) => {
     } catch (err) {
         res.status(500).json(err)
     }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email})
+        if(!user) {
+            res.status(401).json({ msg: "Wrong username or password"})
+        }
+
+        const match = await bycrpt.compare(req.body.password , user.password)
+
+
+        if (match) {
+            const { password, ...info } = user._doc;
+            res.status(200).json(info)
+        } else {
+            res.status(401).json({ msg: "Wrong Username or Password"})
+        }
+        
+    }catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
