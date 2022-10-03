81cd689c2c376e9127e61fdfff271b72625caba1
Drew Payton <drewpayton18@gmail.com>
2022-10-02 21:46:11 -0500

    getting rid of some imports that i wasnt using

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,6 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./Home.scss";
-import AcUnitIcon from "@mui/icons-material/AcUnit";
 import Navbar from "../componets/navbar/Navbar";
 import Featured from "../componets/navbar/featured/Featured";
 
