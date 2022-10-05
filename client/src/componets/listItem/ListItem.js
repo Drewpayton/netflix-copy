a93f2fee4019b312ee2653e5661f08c7b6705f06
Drew Payton <drewpayton18@gmail.com>
2022-10-05 08:02:02 -0500

    adding the list for the movies on the home page

---
+++ client/src/componets/listItem/ListItem.js
@@ -0,0 +1,34 @@
+import "./listItem.scss";
+import mowgli from "../../assets/mowgli.PNG";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
+import { useState } from "react";
+
+const ListItem = ({index}) => {
+  const [isHovered, setIsHovered] = useState(false)
+
+  return (
+    <div className="listItem" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} style={{left: isHovered && index * 225 - 50}}>
+      <img src={mowgli} alt="Mowgli" />
+      <div className="itemInfo">
+        <div className="icons">
+            <PlayArrowIcon />
+            <AddIcon />
+            <ThumbUpOutlinedIcon />
+            <ThumbDownOutlinedIcon />
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 mins</span>
+          <span className="limit">Rated E</span>
+          <span>2018</span>
+        </div>
+        <div className="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
+      </div>
+      <div className="genre">Family</div>
+    </div>
+  );
+};
+
+export default ListItem;
--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,12 +1,20 @@
 import "./Home.scss";
 import Navbar from "../componets/navbar/Navbar";
 import Featured from "../componets/navbar/featured/Featured";
+import List from "../componets/list/List";
 
 export default function Home() {
   return (
     <div className="home">
       <Navbar />
       <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 }
