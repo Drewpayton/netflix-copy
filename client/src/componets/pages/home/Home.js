a688daa4b33732c34d0cfbcdc1e62061187cce3c
Drew Payton <drewpayton18@gmail.com>
2022-10-09 18:18:22 -0500

    adding axios to client side and adding the function to get movie or tv serie list for homepage

--- client/src/componets/pages/home/Home.js
+++ client/src/componets/pages/home/Home.js
@@ -2,8 +2,29 @@ import "./Home.scss";
 import Navbar from "../../navbar/Navbar";
 import Featured from "../../navbar/featured/Featured";
 import List from "../../list/List";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home({ type }) {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}&${genre ? "genre=" + genre : ""}`
+        );
+        console.log(res);
+        // setLists(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getRandomLists();
+  }, [type, genre]);
+
   return (
     <div className="home">
       <Navbar />
